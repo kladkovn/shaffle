856a95b5ad4fac431940af176b10cf555b43454d
VOLODYMYR KLADKO <kladkovn@gmail.com>
2023-12-25 14:06:44 +0200

    start prototype travel agency

---
+++ public/dist/js/container/index/index.js
@@ -0,0 +1,108 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/container/index/index.js":
+/*!**************************************!*\
+  !*** ./src/container/index/index.js ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _script_test__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../script/test */ \"./src/script/test.js\");\n/* harmony import */ var _script_test__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_script_test__WEBPACK_IMPORTED_MODULE_0__);\n\nconsole.log('container');\n\n//# sourceURL=webpack://sass/./src/container/index/index.js?");
+
+/***/ }),
+
+/***/ "./src/script/test.js":
+/*!****************************!*\
+  !*** ./src/script/test.js ***!
+  \****************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\src\\\\script\\\\test.js: `Message3` has already been exported. Exported identifiers must be unique. (12:18)\\n\\n\\u001b[0m \\u001b[90m 10 |\\u001b[39m \\u001b[36mconst\\u001b[39m message3 \\u001b[33m=\\u001b[39m \\u001b[32m''\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 11 |\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 12 |\\u001b[39m \\u001b[36mexport\\u001b[39m {message3\\u001b[33m,\\u001b[39m \\u001b[33mMessage3\\u001b[39m\\u001b[33m,\\u001b[39m message2}\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m                   \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n    at constructor (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:356:19)\\n    at Parser.raise (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3223:19)\\n    at Parser.checkDuplicateExports (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13974:14)\\n    at Parser.checkExport (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13916:16)\\n    at Parser.parseExport (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13718:12)\\n    at Parser.parseStatementContent (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12715:27)\\n    at Parser.parseStatementLike (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12588:17)\\n    at Parser.parseModuleItem (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12565:17)\\n    at Parser.parseBlockOrModuleBlockBody (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13189:36)\\n    at Parser.parseBlockBody (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13182:10)\\n    at Parser.parseProgram (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12464:10)\\n    at Parser.parseTopLevel (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12454:25)\\n    at Parser.parse (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:14376:10)\\n    at parse (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:14417:38)\\n    at parser (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\parser\\\\index.js:41:34)\\n    at parser.next (<anonymous>)\\n    at normalizeFile (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transformation\\\\normalize-file.js:64:37)\\n    at normalizeFile.next (<anonymous>)\\n    at run (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transformation\\\\index.js:21:50)\\n    at run.next (<anonymous>)\\n    at transform (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transform.js:22:33)\\n    at transform.next (<anonymous>)\\n    at step (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\gensync\\\\index.js:261:32)\\n    at C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\gensync\\\\index.js:223:11)\\n    at C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\gensync\\\\index.js:189:28\\n    at C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:67:7\\n    at C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\gensync\\\\index.js:113:33\\n    at step (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\gensync\\\\index.js:287:14)\\n    at C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\gensync\\\\index.js:223:11)\\n    at C:\\\\Users\\\\www\\\\Desktop\\\\prakticher\\\\js\\\\node_modules\\\\gensync\\\\index.js:37:40\");\n\n//# sourceURL=webpack://sass/./src/script/test.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/container/index/index.js");
+/******/ 	
+/******/ })()
+;
